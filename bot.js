84dbc2368751240ffae0e3e5cbfea16ec4bf0878
Faraz Ali <farazappy@esportsindia.in>
2018-09-06 16:15:41 +0530

    tts

--- bot.js
+++ bot.js
@@ -338,7 +338,7 @@ bot.on('message', function(user, userId, channelId, message, event) {
 											tts += ele+' ';
 										});
 
-										googleTTS(tts, 'hi-IN', 1)   // speed normal = 1 (default), slow = 0.24
+										googleTTS('hello', 'hi-IN', 1)   // speed normal = 1 (default), slow = 0.24
 										.then(function (url) {
 										  console.log(url); // https://translate.google.com/translate_tts?...
 
