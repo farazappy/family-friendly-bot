8a189e074424f77775396cdfa7a1d7396043e25d
Faraz Ali <farazappy@esportsindia.in>
2018-11-14 13:32:54 +0530

    shubham

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ bot.on('message', function(user, userId, channelId, message, event) {
 				to: channelId,
 				message: user + ' sorry I don\'t serve Christians'
 			});
-		} else if(userId == '287479791139946496') {
+		} else if(userId == '12345') {
 			bot.sendMessage({
 				to: channelId,
 				message: 'Eww, I\'m not serving you.'
